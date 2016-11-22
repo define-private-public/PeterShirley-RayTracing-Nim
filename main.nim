631ecfa194f585945e0447d6740170c87eea49a8
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-11-21 22:53:00 -0500

    Gettin a sort of "sky"

--- main.nim
+++ main.nim
@@ -12,16 +12,36 @@ else:
   output = stdout
 
 
+proc color(r: ray): vec3=
+  let
+    unit_direction = unit_vector(r.direction())
+    t = 0.5 * (unit_direction.y + 1)
+
+  return (1 - t) * newVec3(1, 1, 1) + t * newVec3(0.5, 0.7, 1)
+
+
+
 proc main()=
   let
-    nx = 200
-    ny = 100
+    nx = 200 * 3
+    ny = 100 * 3
 
   output.write("P3\n", nx, " ", ny, "\n255\n")
+
+  let
+    lower_left_corner = newVec3(-2, -1, -1)
+    horizontal = newVec3(4, 0, 0)
+    vertical = newVec3(0, 2, 0)
+    origin = newVec3(0, 0, 0)
+
   for j in countdown(ny - 1, 0):
     for i in countup(0, nx - 1):
       let
-        col = newVec3(i.float / nx.float, j.float / ny.float, 0.2)
+        u = i.float / nx.float
+        v = j.float / ny.float
+        r = newRay(origin, lower_left_corner + (u * horizontal) + (v * vertical))
+        col = color(r)
+
         ir = (255.99 * col.r).int
         ig = (255.99 * col.g).int
         ib = (255.99 * col.b).int
@@ -32,9 +52,3 @@ proc main()=
 # Run Main method
 main()
 
-let
-  r = newRay(newVec3(0, 1, 0), newVec3(0, 2, 0))
-
-echo r.origin()
-echo r.direction()
-echo r.point_at_parameter(5)
