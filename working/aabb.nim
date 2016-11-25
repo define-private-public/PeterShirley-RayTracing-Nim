86c72b3bdb5eba41d433aed2e24b5c066c84f464
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-11-24 23:51:14 -0500

    Switching out AABB::hit() definitions
    The Pixar guy's one is slightly faster

--- working/aabb.nim
+++ working/aabb.nim
@@ -31,45 +31,45 @@ proc newAABB*(a, b: vec3): aabb =
   result.max = b
 
 
-proc hit*(box: aabb, r: ray, tmin, tmax: float): bool =
-  for a in countup(0, 2):
-    let
-      t0 = ffmin((box.min[a] - r.origin()[a]) / r.direction()[a],
-                 (box.max[a] - r.origin()[a]) / r.direction()[a])
-      t1 = ffmax((box.min[a] - r.origin()[a]) / r.direction()[a],
-                 (box.max[a] - r.origin()[a]) / r.direction()[a])
-
-      # Can't reassign tmin and tmax like in C/C++, so giving these a 2 at the end
-      tmin2 = ffmax(t0, tmin)
-      tmax2 = ffmin(t1, tmax)
-
-    if tmax2 <= tmin2:
-      return false
-
-  return true
+#proc hit*(box: aabb, r: ray, tmin, tmax: float): bool =
+#  for a in countup(0, 2):
+#    let
+#      t0 = ffmin((box.min[a] - r.origin()[a]) / r.direction()[a],
+#                 (box.max[a] - r.origin()[a]) / r.direction()[a])
+#      t1 = ffmax((box.min[a] - r.origin()[a]) / r.direction()[a],
+#                 (box.max[a] - r.origin()[a]) / r.direction()[a])
+#
+#      # Can't reassign tmin and tmax like in C/C++, so giving these a 2 at the end
+#      tmin2 = ffmax(t0, tmin)
+#      tmax2 = ffmin(t1, tmax)
+#
+#    if tmax2 <= tmin2:
+#      return false
+#
+#  return true
 
 
 # This proc below was also in the book as an intersection method.  It was
 # developered originally by Andrew Kensler (at Pixar)
-#proc hit*(box: aabb, r: ray, tmin, tmax: float): bool =
-#  for a in countup(0, 2):
-#    var
-#      invD = 1 / r.direction()[a]
-#      t0 = (box.min[a] - r.origin()[a]) * invD
-#      t1 = (box.max[a] - r.origin()[a]) * invD
-#
-#    if invD < 0:
-#      swap(t0, t1)
-#
-#    let
-#      # Can't reassign tmin and tmax like in C/C++, so giving these a 2 at the end
-#      tmin2 = if (t0 > tmin): t0 else: tmin
-#      tmax2 = if (t1 < tmax): t1 else: tmax
-#
-#    if tmax2 <= tmin2:
-#      return false
-#  
-#  return true
+proc hit*(box: aabb, r: ray, tmin, tmax: float): bool =
+  for a in countup(0, 2):
+    var
+      invD = 1 / r.direction()[a]
+      t0 = (box.min[a] - r.origin()[a]) * invD
+      t1 = (box.max[a] - r.origin()[a]) * invD
+
+    if invD < 0:
+      swap(t0, t1)
+
+    let
+      # Can't reassign tmin and tmax like in C/C++, so giving these a 2 at the end
+      tmin2 = if (t0 > tmin): t0 else: tmin
+      tmax2 = if (t1 < tmax): t1 else: tmax
+
+    if tmax2 <= tmin2:
+      return false
+  
+  return true
 
 
 proc surrounding_box*(box0, box1: aabb): aabb =
