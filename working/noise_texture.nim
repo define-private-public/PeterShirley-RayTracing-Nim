54fd36d1cc0587322e2ff30e25eae2b627a70048
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-11-26 21:28:32 -0500

    Added a scale to noise_texture (still broken)

--- working/noise_texture.nim
+++ working/noise_texture.nim
@@ -6,13 +6,20 @@ import perlin
 type
   noise_texture* = ref object of texture
     noise*: perlin
+    scale*: float
 
 
 proc newNoiseTexture*(): noise_texture =
   new(result)
   result.noise = newPerlin()
+  result.scale = 0
+
+
+proc newNoiseTexture(sc: float): noise_texture =
+  result = newNoiseTexture()
+  result.scale = sc
 
 
 method value*(nt: noise_texture, u, v: float, p: vec3): vec3 =
-  return newVec3(1, 1, 1) * nt.noise.noise(p)
+  return newVec3(1, 1, 1) * nt.noise.noise(nt.scale * p)
 
