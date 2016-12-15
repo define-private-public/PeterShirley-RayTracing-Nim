43a7eb3c36d174496f904d7434566f279dd1a015
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-12-14 19:50:16 -0500

    Fixed stb_image micro-wrapper

--- working/stb_image.nim
+++ working/stb_image.nim
@@ -1,5 +1,8 @@
 # This file is a wrapper of a subset for the "stb_image.h" library that is
-# needed for the raytracer
+# needed for the raytracer.
+#
+# Check out https://gitlab.com/define-private-public/stb_image-Nim for a more
+# robust one.
 
 # Required before including the stb_image.h header
 {.emit: """
@@ -7,35 +10,42 @@
 #include "stb_image.h"
 """.}
 
-# Internal function
-proc stbi_load(filename: cstring; x, y, comp: var cint; req_comp: cint): ptr uint8
+
+
+# Internal functions
+proc stbi_image_free(terval_from_stbi_load: ptr)
+  {.importc: "stbi_image_free", noDecl.}
+
+proc stbi_load(filename: cstring; x, y, comp: var cint; req_comp: cint): ptr cuchar 
   {.importc: "stbi_load", noDecl.}
 
-  # TODO use `cuchar` instead of `uint8` for that proc up there
 
 
 ## External function (the Nim friendly version)
 # This will only load the RGB values, into a sequence of uint8 values
 proc stbi_load*(filename: string; x, y, comp: var int; req_comp: int): seq[uint8] =
   # TODO need to free the image pixels after we've converted/copied
-
   var
     width: cint
     height: cint
-    comp2: cint
+    channels: cint
     pixelData: seq[uint8]
 
-  let data = stbi_load(filename.cstring, width, height, comp2, req_comp.cint)
+  let data = stbi_load(filename.cstring, width, height, channels, req_comp.cint)
 
   # Set the data
   x = width.int
   y = height.int
-  comp = comp2.int
+  comp = channels.int
 
   # Copy it over
-  newSeq(pixelData, x * y)
+  newSeq(pixelData, x * y * comp)
   copyMem(pixelData[0].addr, data, pixelData.len)
 
+  # Free leftovers
+  stbi_image_free(data)
+
+  # Return the seuquence
   return pixelData
 
 
