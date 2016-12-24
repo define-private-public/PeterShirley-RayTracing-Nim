36c7f6a40ca00032f65ebfbfd82994510217b7a8
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-12-23 20:32:10 -0500

    stratified the monte carlo pi program
    end of ch.1

--- working/monte_carlo_pi.nim
+++ working/monte_carlo_pi.nim
@@ -1,18 +1,28 @@
 import util
 
 proc main() =
-  let N = 1000
-  var inside_circle = 0
-  
-  for i in countup(0, N - 1):
-    let
-      x = 2 * drand48() - 1
-      y = 2 * drand48() - 1
+  let sqrt_N = 1000
+  var
+    inside_circle = 0
+    inside_circle_stratified = 0
+ 
+  for i in countup(0, sqrt_N - 1):
+    for j in countup(0, sqrt_N - 1):
+      var 
+        x = 2 * drand48() - 1
+        y = 2 * drand48() - 1
 
-    if ((x * x) + (y * y)) < 1:
-      inside_circle += 1
+      if ((x * x) + (y * y)) < 1:
+        inside_circle += 1
 
-  echo "Estimate of Pi = ", (4 * inside_circle).float / N.float
+      x = 2 * ((i.float + drand48()) / sqrt_N.float) - 1
+      y = 2 * ((j.float + drand48()) / sqrt_N.float) - 1
+
+      if ((x * x) + (y * y)) < 1:
+        inside_circle_stratified += 1
+
+  echo "Regular    Estimate of Pi = ", (4 * inside_circle).float / (sqrt_N * sqrt_N).float
+  echo "Stratified Estimate of Pi = ", (4 * inside_circle_stratified).float / (sqrt_N * sqrt_N).float
 
 
 main()
