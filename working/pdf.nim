3ab7b629c703916a047fc3524374f0570f0e6ce8
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-12-29 14:08:42 -0500

    made PDF pure virtual (by using exceptions)

--- working/pdf.nim
+++ working/pdf.nim
@@ -6,9 +6,9 @@ type
 
 
 method value*(p: pdf; direction: vec3):float {.base.} =
-  return 0
+  raise newException(Exception, "pdf::value() is a pure virtual method")
 
 
 method generate*(p: pdf):vec3 {.base.} =
-  return newVec3()
+  raise newException(Exception, "pdf::generate() is a pure virtual method")
 
